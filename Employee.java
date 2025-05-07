c78e2403f91a81e1d70ac8c0f345533e4ccf57ff
prapticnayak@gmail.com <prapticnayak@gmail.com>
2025-05-07 19:51:38 +0530

    save to

--- Employee.java
+++ Employee.java
@@ -3,26 +3,28 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String department;
 
    
-   public Employee(String name, int id,  double salary) {
+   public Employee(String name, int id,  double salary,String department) {
        this.name = name;
        this.id = id;
-     
        this.salary = salary;
+       this.department=department;
    }
 
    
    public void displayDetails() {
        System.out.println("Employee ID: " + id);
        System.out.println("Name: " + name);
-      
+       
        System.out.println("Salary: $" + salary);
+       System.out.println("Department: " + department);
    }
 
-   
+
    public static void main(String[] args) {
-       Employee emp = new Employee("John Doe", 101, 75000.0);
+        Employee emp=new Employee("Alice",101,300000.0,"Sales");
        emp.displayDetails();
 
      
